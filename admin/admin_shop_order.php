50c3f855fd1b969b93e3f888d5fd178ec2cc29ef
PujithaRC <pujitharoopa@gmail.com>
2022-12-24 19:38:43 +0530

    changes

--- admin/admin_shop_order.php
+++ admin/admin_shop_order.php
@@ -215,7 +215,7 @@
                         <th scope="col">Order Status</th>
                         <th scope="col">Order Date</th>
                         <th scope="col">Customer Name</th>
-                        <th scope="col">Order Cost</th>
+                        
                         <th scope="col">Action</th>
                     </tr>
                 </thead>
@@ -240,7 +240,7 @@
                         echo $order_time;
                         ?></td>
                         <td><?php echo $row["c_firstname"]." ".$row["c_lastname"];?></td>
-                        <td><?php echo $row["p_amount"]." Rs";?></td>
+                        
                         <td><a href="admin_order_detail.php?orh_id=<?php echo $row["orh_id"]?>" class="btn btn-sm btn-primary">View</a></td>
                     </tr>
                     <?php } ?>
