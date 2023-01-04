90b36554ffe37c0de423fa24c23b884b936c52bc
PujithaRC <pujitharoopa@gmail.com>
2023-01-04 15:49:42 +0530

    changes

--- shop/shop_menu_delete.php
+++ shop/shop_menu_delete.php
@@ -6,7 +6,7 @@
     }
     include('../conn_db.php');
     $f_id = $_GET["f_id"];
-    //DISABLE FOOD ITEM INSTEAD OF DELETE IT
+    
     $delete_query = "DELETE FROM food WHERE f_id = '{$f_id}';";
     $delete_result = $mysqli -> query($delete_query);
     if($delete_result){
