198f43586eaa418bae6c9a203cb7f7c0a74572e8
PujithaRC <pujitharoopa@gmail.com>
2023-01-11 15:18:23 +0530

    changes

--- canteen.sql
+++ canteen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2022 at 05:54 AM
+-- Generation Time: Jan 11, 2023 at 10:44 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -20,47 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `canteen`
 --
-
-DELIMITER $$
---
--- Procedures
---
-CREATE DEFINER=`root`@`localhost` PROCEDURE `customer_order` (IN `order_id` INT(11))   BEGIN
-	SELECT orh.orh_refcode AS reference_code, CONCAT(c.c_firstname,' ',c.c_lastname) AS customer_name, f.f_name AS food_name, ord.ord_buyprice AS buy_price, ord.ord_amount AS amount, ord.ord_note AS order_note,orh.orh_ordertime AS order_time, orh,orh_pickuptime AS pickup_time
-    From order_header orh 
-    INNER JOIN order_detail ord ON orh.orh_id=ord_id
-    INNER JOIN food f ON f.f_id=ord.f_id
-    INNER JOIN customer c ON orh.c_id=c.c_id
-    INNER JOIN shop s ON orh.s_id=s.s_id
-    WHERE orh.orh_id=order_id;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `customer_order_history` (IN `customer_id` INT(11))   BEGIN
-	SELECT orh.orh_refcode AS reference_code, CONCAT(c.c_firstname,' ',c.c_lastname) AS customer_name,
-    s.s_name AS shop_name, orh.orh_ordertime AS order_time, orh.orh_pickuptime AS pickup_time,
-    orh.orh_orderstatus AS order_status
-    FROM order_header orh INNER JOIN customer c ON orh.c_id = c.c_id
-   
-    INNER JOIN shop s ON orh.s_id = s.s_id
-    WHERE c.c_id = customer_id;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `shop_alltime_revenue` (IN `shop_id` INT(11))   BEGIN
-	SELECT SUM(ord.ord_amount*ord.ord_buyprice) AS alltime_revenue 
-    FROM order_header orh INNER JOIN order_detail ord ON orh.orh_id = ord.orh_id
-    INNER JOIN food f ON f.f_id = ord.f_id INNER JOIN shop s ON s.s_id = orh.s_id
-    WHERE s.s_id = shop_id AND orh.orh_orderstatus = 'FNSH';
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `shop_menu_revenue` (IN `shop_id` INT(11))   BEGIN
-	SELECT f.f_name AS food_name, SUM(ord.ord_amount*ord.ord_buyprice) AS menu_revenue
-    FROM order_header orh INNER JOIN order_detail ord ON orh.orh_id = ord.orh_id
-    INNER JOIN food f ON f.f_id = ord.f_id
-    WHERE orh.s_id = shop_id AND orh.orh_orderstatus = 'FNSH'
-    GROUP BY ord.f_id ORDER BY menu_revenue DESC;
-END$$
-
-DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -125,22 +84,22 @@ CREATE TABLE `food` (
 --
 
 INSERT INTO `food` (`f_id`, `s_id`, `f_name`, `f_price`, `f_todayavail`, `f_preorderavail`, `f_pic`) VALUES
-(1, 1, 'Masala Dosa', '40.00', 1, 1, 'mdosa.jpg'),
-(2, 1, 'Plain dosa', '30.00', 1, 1, 'pdosa.jpg'),
-(3, 1, 'Idli', '30.00', 1, 1, 'idli.jpg'),
-(4, 1, 'Vade', '10.00', 1, 1, 'vade.jpg'),
-(5, 1, 'Palav', '30.00', 1, 1, 'palav.jpeg'),
-(6, 1, 'Parota', '30.00', 1, 1, 'parota.jpg'),
-(7, 2, 'Veg Sandwich', '40.00', 1, 1, 'sandwich.jpg'),
-(8, 2, 'Corn Sandwich', '40.00', 1, 1, 'corn sandwich.jpg'),
-(9, 2, 'Maggi', '30.00', 1, 1, 'maggi.jpg'),
-(10, 2, 'Cold coffee', '20.00', 1, 1, 'cold-coffee.jpg'),
-(11, 3, 'Meals', '70.00', 1, 1, 'Lunch Thali.jpg'),
-(12, 3, 'Set Dosa', '30.00', 1, 1, 'Set dosa.jpg'),
-(13, 3, 'Pakoda', '15.00', 1, 1, 'pakoda.jpg'),
-(14, 3, 'Puri', '30.00', 1, 1, 'poori.jpg'),
-(15, 3, 'Rava Idli', '30.00', 1, 1, 'Rava idle.jpg'),
-(16, 3, 'Curd rice', '30.00', 1, 1, 'curd rice.jpg');
+(1, 1, 'Masala Dosa', '40.00', 1, 1, '1_1.jpg'),
+(2, 1, 'Plain dosa', '30.00', 1, 1, '2_1.jpg'),
+(3, 1, 'Idli', '30.00', 1, 1, '3_1.jpg'),
+(4, 1, 'Vade', '10.00', 1, 1, '4_1.jpg'),
+(5, 1, 'Palav', '30.00', 1, 1, '5_1.jpeg'),
+(6, 1, 'Parota', '30.00', 1, 1, '6_1.jpg'),
+(7, 2, 'Veg Sandwich', '40.00', 1, 1, '7_2.jpg'),
+(8, 2, 'Corn Sandwich', '40.00', 1, 1, '8_2.jpg'),
+(9, 2, 'Maggi', '30.00', 1, 1, '9_2.jpg'),
+(10, 2, 'Cold coffee', '20.00', 1, 1, '10_2.jpg'),
+(11, 3, 'Meals', '70.00', 1, 1, '11_3.jpg'),
+(12, 3, 'Set Dosa', '30.00', 1, 1, '12_3.jpg'),
+(13, 3, 'Pakoda', '15.00', 1, 1, '13_3.jpg'),
+(14, 3, 'Puri', '30.00', 1, 1, '14_3.jpg'),
+(15, 3, 'Rava Idli', '30.00', 1, 1, '15_3.jpg'),
+(16, 3, 'Curd rice', '30.00', 1, 1, '16_3.jpg');
 
 -- --------------------------------------------------------
 
@@ -162,7 +121,19 @@ CREATE TABLE `order_detail` (
 --
 
 INSERT INTO `order_detail` (`ord_id`, `orh_id`, `f_id`, `ord_amount`, `ord_buyprice`, `ord_note`) VALUES
-(11, 10, 8, 1, '40.00', '');
+(11, 10, 8, 1, '40.00', ''),
+(12, 11, 9, 1, '30.00', ''),
+(13, 12, 1, 1, '40.00', ''),
+(14, 13, 8, 2, '40.00', ''),
+(15, 14, 11, 1, '70.00', ''),
+(16, 15, 9, 1, '30.00', ''),
+(17, 16, 6, 1, '30.00', ''),
+(18, 17, 13, 2, '15.00', ''),
+(19, 18, 3, 1, '30.00', ''),
+(20, 19, 8, 1, '40.00', ''),
+(21, 20, 6, 1, '30.00', ''),
+(22, 21, 7, 1, '40.00', ''),
+(23, 22, 11, 1, '70.00', 'extra sambar');
 
 -- --------------------------------------------------------
 
@@ -186,7 +157,19 @@ CREATE TABLE `order_header` (
 --
 
 INSERT INTO `order_header` (`orh_id`, `orh_refcode`, `c_id`, `s_id`, `orh_ordertime`, `orh_pickuptime`, `orh_orderstatus`, `orh_finishedtime`) VALUES
-(10, '202212220000010', 1, 2, '2022-12-22 04:39:41', '2022-12-22 10:15:00', 'FNSH', '2022-12-22 10:10:45');
+(10, '202212220000010', 1, 2, '2022-12-22 04:39:41', '2022-12-22 10:15:00', 'FNSH', '2022-12-22 10:10:45'),
+(11, '202212240000011', 1, 2, '2022-12-24 05:52:19', '2022-12-24 11:31:00', 'FNSH', '2022-12-24 11:30:40'),
+(12, '202212240000012', 1, 1, '2022-12-24 06:01:52', '2022-12-24 11:35:00', 'FNSH', '2022-12-24 11:32:22'),
+(13, '202212280000013', 1, 2, '2022-12-28 10:55:14', '2022-12-28 16:30:00', 'FNSH', '2022-12-28 16:42:39'),
+(14, '202301050000014', 7, 3, '2023-01-05 07:28:35', '2023-01-05 13:10:00', 'FNSH', '2023-01-05 13:07:38'),
+(15, '202301050000015', 6, 2, '2023-01-05 07:29:11', '2023-01-05 13:05:00', 'FNSH', '2023-01-05 13:03:23'),
+(16, '202301060000016', 7, 1, '2023-01-06 09:23:52', '2023-01-06 15:00:00', 'FNSH', '2023-01-06 14:59:18'),
+(17, '202301070000017', 1, 3, '2023-01-07 06:28:26', '2023-01-07 12:05:00', 'FNSH', '2023-01-07 12:05:22'),
+(18, '202301070000018', 7, 1, '2023-01-07 06:35:04', '2023-01-07 12:10:00', 'FNSH', '2023-01-07 12:08:27'),
+(19, '202301070000019', 6, 2, '2023-01-07 06:37:37', '2023-01-07 12:15:00', 'FNSH', '2023-01-07 12:14:40'),
+(20, '202301090000020', 6, 1, '2023-01-09 05:09:09', '2023-01-09 11:00:00', 'FNSH', '2023-01-09 10:58:02'),
+(21, '202301090000021', 7, 2, '2023-01-09 05:09:48', '2023-01-09 11:00:00', 'FNSH', '2023-01-09 10:58:17'),
+(22, '202301090000022', 1, 3, '2023-01-09 10:26:33', '2023-01-09 16:10:00', 'FNSH', '2023-01-09 16:12:18');
 
 -- --------------------------------------------------------
 
@@ -214,9 +197,9 @@ CREATE TABLE `shop` (
 --
 
 INSERT INTO `shop` (`s_id`, `s_username`, `s_pwd`, `s_name`, `s_location`, `s_openhour`, `s_closehour`, `s_status`, `s_preorderstatus`, `s_email`, `s_phoneno`, `s_pic`) VALUES
-(1, 'Canteen1', '9c740c9ae927a5793fe29170264736e4', 'GJB CANTEEN', 'GJB', '10:00:00', '17:00:00', 1, 1, 'canteen1@gmail.com', '9999999999', 'canteen1.jpg'),
-(2, 'Canteen2', 'eafaac93a6fb9405a96c002c246357ee', 'NRN CANTEEN', 'NRN', '10:00:00', '18:00:00', 1, 1, 'canteen2@gmail.com', '8888888888', 'canteen2.jpg'),
-(3, 'Canteen3', '74074d1bf9ba65f12eb3cf1010d3b644', 'Srikantu Canteen', 'Admin Block', '10:00:00', '18:00:00', 1, 1, 'canteen3@gmail.com', '7777777777', 'canteen3.jpg');
+(1, 'Canteen1', '9c740c9ae927a5793fe29170264736e4', 'GJB CANTEEN', 'GJB', '10:00:00', '17:00:00', 1, 1, 'canteen1@gmail.com', '9999999999', 'shop1.jpg'),
+(2, 'Canteen2', 'eafaac93a6fb9405a96c002c246357ee', 'NRN CANTEEN', 'NRN', '10:00:00', '18:00:00', 1, 1, 'canteen2@gmail.com', '8888888888', 'shop2.jpg'),
+(3, 'Canteen3', '74074d1bf9ba65f12eb3cf1010d3b644', 'Srikantu Canteen', 'Admin Block', '10:00:00', '18:00:00', 1, 1, 'canteen3@gmail.com', '7777777777', 'shop3.jpg');
 
 --
 -- Indexes for dumped tables
@@ -276,7 +259,7 @@ ALTER TABLE `shop`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `ct_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `ct_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `customer`
@@ -294,13 +277,13 @@ ALTER TABLE `food`
 -- AUTO_INCREMENT for table `order_detail`
 --
 ALTER TABLE `order_detail`
-  MODIFY `ord_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `ord_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `order_header`
 --
 ALTER TABLE `order_header`
-  MODIFY `orh_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `orh_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `shop`
